b8dc22602e370b15e2eb91dcee4b38bbaca6778b
ermahechap <ermahechap@unal.edu.co>
2018-05-19 22:20:06 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,37 @@
+
+
+# NYU Stern School of Business - Rooms
+
+> Keywords: Mapas, Visualización, Localización
+
+**Descripción datasets:**
+
+ - [Neighborhood Names GIS][https://catalog.data.gov/dataset/neighborhood-names-gis][Json] Posee informacion de los barrios de NY.
+ - [NY Districts geoshapes][https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson][GeoJson] Posee información de los Distritos de NY y sus formas (polígonos).
+ - [Crimes in NY][https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i/data][Json]Base de datos con reportes de crímenes en NY con lugar de ocurrencia(lat,long), entidad que lo reporta, hora de ocurrencia y una descripción  breve del suceso.
+ - [Dataset contains information on New York City housing by building data][https://catalog.data.gov/dataset/housing-new-york-units-by-building][Json] Información de viviendas asequibles. Dicha información incluye su ubicación.
+
+Se mejora la visualizacion en los mapas, se espera que para la proxima entrega se realizen tablas para visualizar informacion y generar la descarga de csv, y en la ultima entrega incluir graficos con 3djs.
+*Nota: ver javascript.*
+
+**Descripción breve:**
+El proyecto es un mashup que incluye un mapa para visualizar información y ordenarla. El mapa incluye una sección en la que se van a añadir gráficos de D3.js que muestren el desempeño del distrito seleccionado. Aquí se pueden seleccionar los barrios a comparar en siguiente sección.
+
+En la sección de comparación se pretende generar una vista para comparar varios barrios/viviendas seleccionadas en el mapa, y determinar que valores se quieren considerar para clasificarlas.
+
+En la sección de descargas se le permite al usuario generar un informe con los datos que el desee incluir y generar un csv.
+
+**Descripción:**
+-   Map View:
+    1.  [Y] El mapa esta centrado sobre la NYU Stern School of Business.
+    2.  [Y] [Boroughs]El mapa hasta el momento incluye una vista basica de los boroughs y los permite cargar independientemente, y permite tambien seleccionar por distritos y decidir que informacion desea mostrar(museos, viviendas, barrios,etc).
+    3. [N] Se puede explorar de forma diferente el mapa haciendo uso de las pestañas incluidas en la barra de opciones del mapa. Cada pestaña permite visualizar los datos de forma diferente.
+-   Data Visualization:
+
+    1.  [N]Aún no hay uso de gráficos, sin embargo ya hay una parte dispuesta para ello.
+-   Interaction Form:
+
+    1.  [Y]El mapa ya dispone de las funcionalidades deseadas para visualizar de formas diferentes el mapa.
+
+6.  Test Case:
+Hasta el momento se ha probado en Firefox y Chrome, y el desempeño de la aplicación ha sido similar. También se ha probado la aplicación con las funcionalidades de estos navegadores para simular un ambiente móvil, y se ha demostrado que funciona responsivamente.
---
+++ scripts.js
@@ -0,0 +1,498 @@
+/*Ranking Script*/
+/* --------------------- Load the datasets -------------------------*/
+
+var URLS = [
+    {name: "geoshapes", url:"https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson"},
+    {name: "neighborhood_names", url:"https://data.cityofnewyork.us/api/views/xyye-rtrs/rows.json?accessType=DOWNLOAD"},
+    {name: "housing", url:"https://data.cityofnewyork.us/api/views/hg8x-zxpr/rows.json?accessType=DOWNLOAD"},
+    {name: "museums", url:"https://data.cityofnewyork.us/api/views/fn6f-htvy/rows.json?accessType=DOWNLOAD"},
+    {name: "art", url:"https://data.cityofnewyork.us/api/views/43hw-uvdj/rows.json?accessType=DOWNLOAD"},
+    {name: "crime", url:"https://data.cityofnewyork.us/api/views/qgea-i56i/rows.json?accessType=DOWNLOAD"}
+];
+
+var boroR = {"Manhattan":"1","Bronx":"2","Brooklyn":"3","Queens":"4","Staten Island":"5"};
+
+var boroughs = { // in this one we build the "dataframe"
+    "1": {boro_name:"Manhattan",color: "#06e908",districts:[]},//1
+    "2": {boro_name:"Bronx",color: "#134bdb",districts:[]},//2
+    "3": {boro_name:"Brooklyn",color:"#d7480b",districts:[]},//3
+    "4": {boro_name:"Queens",color: "#c211ee",districts:[]},//4
+    "5": {boro_name:"Staten Island",color: "#dbd318",districts:[]}//5
+};
+
+var geoshapesData,neighborhoodData,housingData,museumsData,artData,crimeData;
+
+
+function loadBasicInfo(){
+    console.log("loading geoshapes...");
+    $.getJSON(URLS[0].url,function(data){//geoshapes
+        let len = data.features.length;
+        for(let i = 0 ; i <len;i++){
+            let boroCD = data.features[i].properties.BoroCD/100.0>>0;
+            let boroID = data.features[i].properties.BoroCD - (boroCD*100);
+            let mPol = createPolygon(data.features[i].geometry)
+            mPol.setOptions({
+                fillColor:boroughs[boroCD].color,
+                strokeColor:"#5a5e4b",
+                strokeOpacity:0.5,
+                strokeWeight: 2
+            });
+             //Template of values for each boro
+            boroughs[boroCD].districts.push({
+                number:boroID,
+                poly: data.features[i].geometry,
+                mapsPolygon: mPol,//polygon
+                borough_center: mPol.my_getBounds().getCenter(),
+                neighborhoods: [],
+                housing: [],
+                museums: [],
+                art: [],
+                crimes: [],
+                number_crimes: 0,
+                number_units: 0,
+                distance: 0,
+                number_museums_art: 0
+            });
+        }
+    }).fail(function(){
+        alert("Couldn't load a dataset. please reload the page!");
+    }).done(function(){
+        console.log("loading neighborhoods...");
+        $.getJSON(URLS[1].url,function(data){ //Neighborhood names
+            let lenData = data.data.length;
+            for(let i = 0 ; i < lenData; i++){
+                let id = boroR[data.data[i][16]];
+                let point = parser(data.data[i][9]);
+                let lenDist = boroughs[id].districts.length;
+                for(let j = 0 ; j < lenDist ; j++){
+                    if(inOutMapsQuery(point,boroughs[id].districts[j].mapsPolygon)){
+                        boroughs[id].districts[j].neighborhoods.push({
+                            name:data.data[i][10],
+                            location:point,
+                            mapsLocation: new google.maps.Marker({
+                                position:point,
+                                icon:createIcon("src/neighborhood.png"),
+                                title: data.data[i][10]
+                            })
+                        });
+                        break;
+                    }
+                }
+            }
+        }).done(function(){
+            console.log("loading other assets...");
+            loadHousing();
+            loadMuseums();
+        }).fail(function(){alert("Couldn't load a dataset. please reload the page!");});
+
+    });
+}
+
+function loadHousing(){
+    $.getJSON(URLS[2].url,function(data){ // housing, note, I filter housing and only consider houses with lat lng defined
+        let lenData = data.data.length;
+        for(let i = 0 ; i < lenData; i++){
+            let id = boroR[data.data[i][15]];
+            if(data.data[i][23]==null)continue; // exclude housese without lat lng
+            let point = {lat: parseFloat(data.data[i][23]) , lng:parseFloat(data.data[i][24])};
+            let lenDist = boroughs[id].districts.length;
+            for(let j = 0 ; j < lenDist ; j++){
+                if(inOutMapsQuery(point,boroughs[id].districts[j].mapsPolygon)){
+                    boroughs[id].districts[j].housing.push({
+                        name:data.data[i][9],
+                        location:point,
+                        mapsLocation: new google.maps.Marker({
+                            position:point,
+                            icon: createIcon("src/house.png"),
+                            title: data.data[i][9]
+                        })
+                    });
+                    boroughs[id].districts[j].number_units += parseInt(data.data[i][31]);//add low income units
+                    break;
+                }
+            }
+        }
+    }).done(function(){
+        console.log("Housing loaded...");
+    }).fail(function(){alert("Couldn't load a dataset. please reload the page!");});
+}
+
+function loadMuseums(){
+    $.getJSON(URLS[3].url,function(data){
+        let lenData = data.data.length;
+        for(let i = 0 ; i < lenData ; i++){
+            let id = -1;
+            let point = parser(data.data[i][8]);
+            //quite greedy, but there is no other way but check all districts because the data has no district info
+            for(let j = 1 ; j <=5 ; j++){//boro
+                let lenDist = boroughs[j].districts.length;
+                for(let k = 0 ; k < lenDist ; k++){//district
+                    if(inOutMapsQuery(point,boroughs[j].districts[k].mapsPolygon)){
+                        boroughs[j].districts[k].museums.push({
+                            name: data.data[i][9],
+                            location:point,
+                            mapsLocation: new google.maps.Marker({
+                                position:point,
+                                icon: createIcon("src/museum.png"),
+                                title:data.data[i][9]
+                            }),
+                            address: data.data[i][12] + " - " + data.data[i][13],
+                            tel: data.data[i][10],
+                            url: data.data[i][11]
+                        })
+                    }
+                }
+            }
+            if(id == -1)continue;
+        }
+    }).done(function(){
+        console.log("Museums loaded...");
+    }).fail(function(){alert("Couldn't load a dataset. please reload the page!");});
+}
+
+//this is temporal
+var heatMapData = [];
+var heatmap;
+$.ajax({
+    url: "https://data.cityofnewyork.us/resource/9s4h-37hy.json?$where=latitude IS NOT NULL AND longitude IS NOT NULL",
+    type: "GET",
+    data: {
+      "$limit" : 5000,
+      "$$app_token" : "LsbMCOtwH1ZSzEhm10cXMFk1U"
+    }
+}).done(function(data) {
+    crimeData = data;
+    let dtSize = data.length;
+    for(let i = 0 ; i <dtSize;i++){
+        heatMapData.push(new google.maps.LatLng(data[i].latitude,data[i].longitude));
+    }
+    heatmap = new google.maps.visualization.HeatmapLayer({
+        data: heatMapData
+    });
+    console.log(data);
+});
+
+/*---------------------------- Google maps interaction scripts ----------------- */
+
+var mapTop;
+var coordNYU = {lat: 40.729218, lng: -73.996492};
+
+function createIcon(ico_url){
+    return{
+        url: ico_url,
+        scaledSize: new google.maps.Size(40, 40), //scale
+        origin: new google.maps.Point(0,0),
+        anchor: new google.maps.Point(0, 0)
+    };
+}
+
+function parser(str){
+   str = str.split(" ");
+   return {lat: parseFloat( str[2].slice(0,-1) ) , lng: parseFloat( str[1].slice(1) ) };
+}
+function latLongMaps(dat){
+    return {lat:dat[1] , lng: dat[0]};
+}
+
+function inOutMapsQuery(point, polygon){ //slower
+    let pun = new google.maps.LatLng(point.lat,point.lng);
+    return google.maps.geometry.poly.containsLocation(pun,polygon);
+}
+
+function inOutNonMaps(point, polygon){ //both points and polygon are just objects, not maps elements
+
+}
+
+
+function createPolygon(poly){
+    let dt = [],pol;
+    if(poly.type == "Polygon"){
+        let len  = poly.coordinates[0].length;
+        for(let i = 0 ; i <len;i++){
+            dt.push(latLongMaps(poly.coordinates[0][i]));
+        }
+        pol = new google.maps.Polygon({
+            paths: dt
+        });
+
+    }else{
+        let len = poly.coordinates.length;
+        for (let i = 0 ; i < len;i++){
+            let dt2 = [],len2 = poly.coordinates[i][0].length;
+            for(j = 0 ; j<len2;j++){
+                dt2.push(latLongMaps(poly.coordinates[i][0][j]));
+            }
+            dt.push(dt2);
+        }
+        pol = new google.maps.Polygon({
+            paths:dt
+        });
+    }
+    return pol;
+}
+
+
+
+function onGoogleMapResponse(){
+    //Extend Polygon
+    //taken from https://stackoverflow.com/questions/3081021/how-to-get-the-center-of-a-polygon-in-google-maps-v3?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+    google.maps.Polygon.prototype.my_getBounds=function(){
+        var bounds = new google.maps.LatLngBounds();
+        this.getPath().forEach(function(element,index){bounds.extend(element)});
+        return bounds;
+    }
+
+    mapTop = new google.maps.Map(document.getElementById('mapContainer'),{
+        center: coordNYU,
+        zoom: 10,
+        gestureHandling: 'greedy', // avoid ctrl + scroll
+        zoomControl: true,
+        zoomControlOptions:{
+            position: google.maps.ControlPosition.LEFT_BOTTOM
+        },
+        fullscreenControl: false,
+        mapTypeControl: false,
+        streetViewControl: false
+    });
+    var fixedMarker = new google.maps.Marker({
+        position: coordNYU,
+        icon: createIcon("src/uni.png"),
+        map: mapTop,
+        title: 'NYU!',
+    });
+    console.log("map");
+}
+
+function showBoros(id){
+    let lenDist = boroughs[id].districts.length;
+    for(let i = 0 ; i < lenDist;i++){
+        boroughs[id].districts[i].mapsPolygon.setMap(mapTop);
+    }
+}
+function hideBoros(id){
+    let lenDist = boroughs[id].districts.length;
+    for(let i = 0 ; i < lenDist;i++){
+        boroughs[id].districts[i].mapsPolygon.setMap(null);
+    }
+}
+
+
+function focusDistrict(id_boro,id_district){
+    boroughs[id_boro].districts[id_district].mapsPolygon.setOptions({
+        strokeColor:"#ff1f00",
+        strokeOpacity: 1.0,
+        strokeWeight:5
+    });
+    mapTop.setOptions({
+        center: boroughs[id_boro].districts[id_district].borough_center,
+        zoom: 13
+    });
+    boroughs[id_boro].districts[id_district].mapsPolygon.setMap(mapTop);
+}
+function lostFocusDistrict(id_boro,id_district){//resets default colors.
+    boroughs[id_boro].districts[id_district].mapsPolygon.setOptions({
+        strokeColor:"#5a5e4b",
+        strokeOpacity:0.5,
+        strokeWeight: 2
+    });
+}
+
+function showHideMarkers(id_boro,id_district,active){
+    let len = boroughs[id_boro].districts[id_district].housing.length
+    if(active&1)
+        for(let i = 0 ; i < len ; i++){boroughs[id_boro].districts[id_district].housing[i].mapsLocation.setMap(mapTop);}
+    else
+        for(let i = 0 ; i < len ; i++){boroughs[id_boro].districts[id_district].housing[i].mapsLocation.setMap(null);}
+
+    len = boroughs[id_boro].districts[id_district].neighborhoods.length
+    if(active&2)
+        for(let i = 0 ; i < len ; i++){boroughs[id_boro].districts[id_district].neighborhoods[i].mapsLocation.setMap(mapTop);}
+    else
+        for(let i = 0 ; i < len ; i++){boroughs[id_boro].districts[id_district].neighborhoods[i].mapsLocation.setMap(null);}
+
+    len = boroughs[id_boro].districts[id_district].museums.length
+    if(active&4)
+        for(let i = 0 ; i < len ; i++){boroughs[id_boro].districts[id_district].museums[i].mapsLocation.setMap(mapTop);}
+    else
+        for(let i = 0 ; i < len ; i++){boroughs[id_boro].districts[id_district].museums[i].mapsLocation.setMap(null);}
+}
+
+function showAllBoroInfo(id_boro,mask_markers,opt){
+    let lenDist = boroughs[id_boro].districts.length;
+    for(let i = 0 ; i < lenDist;i++){
+        boroughs[id_boro].districts[i].mapsPolygon.setMap((opt)?mapTop:null);
+        if (opt)showHideMarkers(id_boro,i,mask_markers);
+        else showHideMarkers(id_boro,i,0);
+    }
+}
+
+function showHideBorosAndMarkers(mask_markers,mask_boros){ // note, this function receives a bitmask
+    for(let i = 1,j=1 ; i <=16;i*=2,j++){
+        showAllBoroInfo(j, mask_markers, mask_boros&i);
+    }
+}
+
+
+/*-----------------Interactions ------------------*/
+/*-- Buttons and other interactions -- */
+/*Load and wait */
+$(document).ready(function(){
+    for(var i = 1 ; i<=5;i++){
+        $('#boro-map').append('<option value="' + i + '">' + boroughs[i].boro_name+ '</option>');
+    }
+});
+var MyVar;
+function waitLoad(){
+    loadBasicInfo();
+
+    myVar = setTimeout(showPage, 100);
+}
+
+function showPage() {
+  document.getElementById("load-screen").style.display = "none";
+  document.getElementById("containter-whole").style.display = "block";
+}
+
+/*Map interactions*/
+
+//Detect tabs change in the tabs of the map and reset form
+var currentMapTab = 0;
+
+function resetForm(){
+    if(currentMapTab == 0){
+        if(boroughChosen != 0 || districtChosen != 0){
+            let notClick = true;
+            if(document.getElementById('housing-enable').checked == 0 && document.getElementById('neighborhood-enable').checked ==0 && document.getElementById('museum-enable').checked==0)notClick=true;
+            document.getElementById('housing-enable').checked = document.getElementById('neighborhood-enable').checked = document.getElementById('museum-enable').checked = 0;
+            if(!notClick)$("#show-data-map").trigger("click");
+            document.getElementById('boro-map').value = 0;
+            $("#boro-map").trigger("change");
+        }
+    }else if(currentMapTab == 1){
+
+    }else{
+
+    }
+}
+
+$("a[href='#find-tab']").on('shown.bs.tab', function(e) {
+    resetForm();
+    currentMapTab = 0;
+});
+
+$("a[href='#explore-tab']").on('shown.bs.tab', function(e) {
+    resetForm();
+    currentMapTab = 1;
+});
+$("a[href='#selected-tab']").on('shown.bs.tab', function(e) {
+    resetForm();
+    currentMapTab = 2;
+});
+
+
+//----- Map find tab ----
+var boroughChosen = 0;
+var districtChosen = 0;
+var activeMarkers = 0; //bitmask
+$("#boro-map").change(function(){
+    if(this.value != 0){
+        mapTop.setOptions({center: coordNYU,zoom: 10});//resets
+        showBoros(this.value);
+        if(districtChosen!=0){
+            lostFocusDistrict(boroughChosen,districtChosen-1);
+            showHideMarkers(boroughChosen,districtChosen-1,0);
+            boroughs[boroughChosen].districts[districtChosen-1].mapsPolygon.setMap(null);
+            districtChosen = 0; //resets
+        }
+        if(boroughChosen!=0)hideBoros(boroughChosen);
+        boroughChosen=this.value;
+        $("#boro-dic-map").empty();
+        $("#boro-dic-map").append('<option value="0">Choose...</option>');
+        let lenDist = boroughs[boroughChosen].districts.length;
+        for(let i = 1 ; i <=lenDist;i++){
+            $('#boro-dic-map').append('<option value="' + i + '">' + boroughs[boroughChosen].districts[i-1].number+ '</option>');
+        }
+        $("#boro-dic-map").prop("disabled",false);
+    }else{
+        hideBoros(boroughChosen);
+        if(districtChosen!=0){
+            lostFocusDistrict(boroughChosen,districtChosen-1);
+            showHideMarkers(boroughChosen,districtChosen-1,0);
+            districtChosen = 0;
+        }
+        boroughChosen = 0;
+        $("#boro-dic-map").empty();
+        $("#boro-dic-map").append('<option value="0">Choose...</option>');
+        $("#boro-dic-map").prop("disabled",true);
+    }
+});
+
+$("#boro-dic-map").change(function(){
+    if(this.value != 0){
+        focusDistrict(boroughChosen,this.value-1);
+        showHideMarkers(boroughChosen,this.value-1,activeMarkers);
+        if(districtChosen != 0){
+            showHideMarkers(boroughChosen,districtChosen-1,0);
+            lostFocusDistrict(boroughChosen,districtChosen-1);
+        }
+        districtChosen = this.value;
+    }else{
+        lostFocusDistrict(boroughChosen,districtChosen-1);
+        showHideMarkers(boroughChosen,districtChosen-1,0);
+        districtChosen = 0;
+        mapTop.setOptions({center: coordNYU,zoom: 10});//resets
+    }
+});
+
+$("#show-data-map").click(function(){
+    if(districtChosen == 0){
+        alert("First pick a borough and a disrict number");
+        return;
+    }
+    activeMarkers = 0;
+    $("input:checkbox[name = data-pick]:checked").each(function(){
+        activeMarkers+=parseInt($(this).val());
+    });
+    showHideMarkers(boroughChosen,districtChosen-1,activeMarkers); // activeMarkers = 0 to hidde all
+});
+
+
+//----- Map explore tab ----
+var activeBoros = 0; //bitmask
+var activeBrMarkers = 0;
+var heatMapCrime = 0;
+//show boroughs
+$("#show-borough").click(function(){
+    activeBoros = 0;
+    $("input:checkbox[name = borough-pick]:checked").each(function(){
+        activeBoros+=parseInt($(this).val());
+    });
+    showHideBorosAndMarkers(activeBrMarkers,activeBoros);
+});
+
+
+//show markers
+$("#show-data-explore").click(function(){
+    activeBrMarkers = 0;
+    $("input:checkbox[name = borough-pick-2]:checked").each(function(){
+        activeBrMarkers+=parseInt($(this).val());
+    });
+    showHideBorosAndMarkers(activeBrMarkers,activeBoros);
+});
+
+//hetmapEnable,diable
+$("#heat-crime").click(function(){
+    if(heatMapCrime == 0){
+        heatmap.setMap(mapTop);
+        heatMapCrime = 1;
+    }else{
+        heatmap.setMap(null);
+        heatMapCrime = 0;
+    }
+});
+
+
+/*Padding for the navigation bar*/
+$("#navigationMenu").resize(function(){
+    $("navBarSpacing").height($("#navigationMenu").height()+10);
+});
